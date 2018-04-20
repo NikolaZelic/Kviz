e84e7e3d3cece6243c1e61784aeae5f607f07be4
nikolazelic <nzelic@ymail.com>
2018-04-20 13:53:30 +0200

    1.2

---
+++ Kviz/build/web/login.js
@@ -0,0 +1,31 @@
+function login() {
+    var userOK = false;
+    var passwordOK = false;
+    var user = $("#user").val();
+    var password = $("#password").val();
+    if (user.length > 2) {
+        $("#error").hide();
+    } else {
+        $("#error").html("Username too short");
+        $("#error").show();
+        return;
+    }
+    $.post("proverikorisnika", {"user": user, "password": password}, function (data) {
+        if (data.user.toString().trim() == "1") {
+            userOK = true;
+        }
+        if (data.password.toString().trim() == "1") {
+            passwordOK = true;
+        }
+        if (!userOK) {
+            $("#error").show();
+            $("#error").html("Username incorrect");
+        } else if (!passwordOK) {
+            $("#error").show();
+            $("#error").html("Password incorrect");
+        } else {
+            $("#error").hide();
+            window.location.href = "http://localhost:8080/Kviz/pitanja.html";
+        }
+    });
+}
