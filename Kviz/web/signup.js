02253856ca842c575d7d3aa66c6b27d9badbbfd0
nikolazelic <nzelic@ymail.com>
2018-04-20 16:15:22 +0200

    bolji sign up

--- Kviz/web/signup.js
+++ Kviz/web/signup.js
@@ -1,18 +1,19 @@
 $(document).ready(
-        $("#buttS").click(function(){
-            var user = $("#nameS").val();
-            var password = $("#nameS").val();
-            
-        
-            $.post("proverikorisnika",{"user":user},function (data,status){
-                if(rata.user != "1")return ;
-            });
-                
-            $.post("signup",{"user":nesto,"password":password},function (data,status){
-                
-                    window.location.href = "http://localhost:8080/Kviz/pitanja.html";
-                });
-            }));
-               
-       
-        
+        $("#buttS").click(function () {
+    var user = $("#nameS").val();
+    var password = $("#nameS").val();
+
+
+    $.post("proverikorisnika", {"user": user}, function (data, status) {
+        if (rata.user != "1")
+            return;
+    });
+
+    $.post("signup", {"user": nesto, "password": password}, function (data, status) {
+
+        window.location.href = "http://localhost:8080/Kviz/pitanja.html";
+    });
+}));
+
+
+
